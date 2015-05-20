0f3b95bf70c8dc20de1384ea293beff48ffe05b6
WQ <niuyiwangqun@sohu.com>
2015-05-20 13:53:45 +0800

    c

--- example/example.js
+++ example/example.js
@@ -2,7 +2,7 @@
  * Created by acer on 2015/5/20.
  */
 
-var z = require( "../lib/zach-zib.js" );
+var z = require( "../lib/zach-zip.js" );
 var pro = require( "child_process" );
 
 //var last = pro.execFile( "../lib/7z.exe", ["d", "TweenMax.rar", "*.js", "-r", "-p"], function ( err, stdout, stderr ) {
